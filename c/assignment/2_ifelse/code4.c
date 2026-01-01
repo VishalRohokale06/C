2a52d3d0b0e8584e0aaab3c45f4a17293d013808
VishalRohokale06 <vvrohokale16@gmail.com>
2026-01-01 22:55:36 +0530

    if else code

---
+++ c/assignment/2_ifelse/code4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main(){
+	int age;
+
+	printf("Enter age:");
+	scanf("%d",&age);
+
+	if(age>=18){
+
+		printf("Eligible for voting\n");
+	}
+	else{
+
+		printf("Not Eligible for voting\n");
+	}
+}
+
