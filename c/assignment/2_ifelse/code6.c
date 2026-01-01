840283071d69e6c4c361e3d315847541a23ab9fd
VishalRohokale06 <vvrohokale16@gmail.com>
2026-01-01 22:55:51 +0530

    if else code

---
+++ c/assignment/2_ifelse/code6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main(){
+	int num;
+
+	printf("Enter num : ");
+	scanf("%d",&num);
+
+	if(num%3==0 && num%7==0){
+		printf("%d is divisible by 3 and 7\n",num);
+	}
+	else{
+		 printf("%d is not divisible by 3 and 7\n",num);                                                      }
+}
