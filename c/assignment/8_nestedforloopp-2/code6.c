5127c79d2becde4a1f171d6500aa56e363d9becb
VishalRohokale06 <vvrohokale16@gmail.com>
2026-01-04 17:31:54 +0530

    nestedfor loop code

---
+++ c/assignment/8_nestedforloopp-2/code6.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main(){
+
+	int rows;
+	printf("rows=");
+	scanf("%d",&rows);
+
+	int num=1;
+
+	for(int i=1;i<=rows;i++){
+		for(int j=1;j<=rows;j++){
+			printf("%d\t",rows*num++);
+		}
+		printf("\n");
+	}
+}
+			
