c1037de721bda64db563dc7bfb8b190996dd31cf
VishalRohokale06 <vvrohokale16@gmail.com>
2026-01-05 14:35:04 +0530

    triangle pattern code

---
+++ c/assignment/17_Triangle_Pattern/code2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main(){
+
+	int rows;
+	printf("Rows:");
+	scanf("%d",&rows);
+
+	for(int i=1;i<=rows;i++){
+		int ch = 96+i;
+		for(int j=1;j<=i;j++){
+			printf("%c\t",ch);
+			ch--;
+		}
+		printf("\n");
+	}
+}
