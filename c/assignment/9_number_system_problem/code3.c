bf626d75181146fe015714f5551eba45f7d5cede
VishalRohokale06 <vvrohokale16@gmail.com>
2026-01-09 21:05:46 +0530

    number system code

---
+++ c/assignment/9_number_system_problem/code3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void main(){
+
+	int num;
+	printf("Enter num:");
+	scanf("%d",&num);
+        
+	int count=0;
+
+	if(num>1){
+		for(int i=1;i<=num;i++){
+		if(num%i==0){
+			count++;
+		}
+		}
+	}
+
+	if(count>2){
+		printf("Not prime\n");
+	}else{
+		printf("Prime\n");
+	}
+}
