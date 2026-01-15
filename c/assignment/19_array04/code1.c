af70564d17adc765fa5f6d1542ed6edc5057ace7
VishalRohokale06 <vvrohokale16@gmail.com>
2026-01-15 18:55:31 +0530

    array code

---
+++ c/assignment/19_array04/code1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void main(){
+
+	int size;
+	printf("Enter the size of the array:\n");
+	scanf("%d",&size);
+
+	int arr[size];
+	printf("Enter the elements:\n");
+
+	for(int i=0;i<size;i++){
+		scanf("%d",&arr[i]);
+	}
+        iprintf("Prime number present in an array:\n");
+	for(int i=0;i<size;i++){
+		int num=arr[i];
+		int isPrime=1;
+		if(num<=1){
+			isPrime=0;
+		}else{
+		for(int j=2;j<num;j++){
+			if(num%j==0){
+                        isPrime=0;
+			break;
+			}
+		}
+		}
+
+		if(isPrime){
+			printf("%d\n",arr[i]);
+		}
+	}
+}
