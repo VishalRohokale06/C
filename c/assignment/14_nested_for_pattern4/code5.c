7d75bd210b874a40ae83167a63b5d7f5b141a07e
VishalRohokale06 <vvrohokale16@gmail.com>
2026-01-20 22:10:21 +0530

    nested for loop code

---
+++ c/assignment/14_nested_for_pattern4/code5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main(){
+
+	int rows;
+	printf("Rows:");
+	scanf("%d",&rows);
+
+	int num=1;
+	for(int i=1;i<=rows;i++){
+		int val = num;
+		for(int j=1;j<=rows;j++){
+			printf("%d\t",val);
+			val+=num;
+		}
+		num+=rows;
+		printf("\n");
+	}
+}
