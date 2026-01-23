ba4c2f2ca8721cfe9ff069e434831a0288776786
VishalRohokale06 <vvrohokale16@gmail.com>
2026-01-23 13:07:31 +0530

    array code

---
+++ c/assignment/20_array05/code8.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void main(){
+
+	int size;
+	printf("Enter the size of the array:\n");
+	scanf("%d",&size);
+
+	int arr[size];
+	printf("Enter the elements:\n");
+
+	for(int i=0;i<size;i++){
+		scanf("%d",&arr[i]);
+	}
+        int flag=0;
+	for(int i=0;i<size;i++){
+		if(arr[i]>arr[i+1]){
+			flag=1;
+		}else{
+			flag=0;
+			break;
+		}
+	}
+
+	if(flag==1){
+		printf("This is decreasing array.\n");
+	}else{
+		printf("This is not decreasing array.\n");
+	}
+}
