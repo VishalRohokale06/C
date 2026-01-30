bb33adc5d85d57a8fe127e99c33f737d434dc2b4
VishalRohokale06 <vvrohokale16@gmail.com>
2026-01-30 10:18:19 +0530

    array pointer code

---
+++ c/dailycodes/Array_Pointers/code20.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main(){
+	int x,y,z;
+	x=10;
+	y=20;
+	z=30;
+	int *arr[] = {&x,&y,&z};
+
+	for(int i=0;i<3;i++){
+		printf("%p\n",arr[i]);
+		//printf("%d\n",*(arr[i]));
+		printf("%d\n",*(*(arr+i)));
+	}
+}
