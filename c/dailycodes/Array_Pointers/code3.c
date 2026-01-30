ddd91762425bf62b4b445fee8c16499e82f10ebd
VishalRohokale06 <vvrohokale16@gmail.com>
2026-01-30 10:14:39 +0530

    array pointer code

---
+++ c/dailycodes/Array_Pointers/code3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main(){
+
+	int arr[3]={};
+	int arr1[2];
+
+	for(int i=0;i<3;i++){
+		printf("%d\n",arr[i]);
+	}
+
+		printf("%d\n",arr1[1]);
+		printf("%d\n",arr1[2]);
+}
